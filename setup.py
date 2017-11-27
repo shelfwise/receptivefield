2ed646762b53c5d1d452d93b6f0873e03a770af9
kmkolasinski <kmkolasinski@gmail.com>
2017-11-27 21:57:21 +0100

    Add MANIFEST

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup
 
 setup(
   name = 'receptivefield',
-  packages = ['receptivefield'],
-  version = '0.1',
+  packages = ['receptivefield'],  
+  version = '0.1.1',
   description = 'Gradient based Receptive field estimation library',
   author = 'fornax.ai',
   author_email = 'krzysztof.kolasinski@fornax.ai',
   url = 'https://github.com/fornaxai/receptivefield',
   download_url = 'https://github.com/fornaxai/receptivefield/archive/0.1.tar.gz', 
   keywords = ['tensorflow', 'keras', 'receptivefield'],
+  install_requires=[
+     'matplotlib', 'pillow', 'numpy', 'keras'
+  ],
   classifiers = [],
 )
